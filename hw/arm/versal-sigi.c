35560e284cc5c8e50abad6eb91c9ff1e7e98521e
Charleye <wangkart@aliyun.com>
2023-05-18 21:04:50 +0800

    arm: sigi-virt: Add pflash drive map

--- hw/arm/versal-sigi.c
+++ hw/arm/versal-sigi.c
@@ -582,6 +582,12 @@ static void sigi_virt_realize(DeviceState *dev, Error **errp)
         }
         create_sd_card(&s->apu.peri.mmc[i], i);
     }
+
+    /* Map legacy -drive if=pflash to machine properties */
+    for (i = 0; i < ARRAY_SIZE(s->flash); i++) {
+        pflash_cfi01_legacy_drive(s->flash[i],
+                                  drive_get(IF_PFLASH, 0, i));
+    }
 }
 
 static Property sigi_virt_properties[] = {
