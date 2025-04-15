e8d75392cd9f4b342cd3d4ecc308a2d9b466ee99
chasinglulu <wangkart@aliyun.com>
2025-04-15 11:50:22 +0800

    hw/misc: laguna: Add support for register with rstval

---
+++ hw/misc/axera-register.c
@@ -0,0 +1,109 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ * @Description:
+ *
+ * Copyright (C) 2025 chasinglulu <wangkart@aliyun.com>
+ *
+ */
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Axera Laguna SoC register emulation
+ *
+ * Copyright (C) 2025 chasinglulu <wangkart@aliyun.com>
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/units.h"
+#include "hw/sysbus.h"
+#include "migration/vmstate.h"
+#include "hw/qdev-properties.h"
+#include "qemu/log.h"
+#include "qemu/module.h"
+#include "qemu/error-report.h"
+#include "qapi/error.h"
+
+struct LUARegisterState {
+	SysBusDevice parent_obj;
+
+	MemoryRegion mmio;
+	char *name;
+
+	/* register default value*/
+	uint32_t rstval;
+};
+
+typedef struct LUARegisterState LUARegisterState;
+#define TYPE_LUA_REGISTER "laguna.register"
+DECLARE_INSTANCE_CHECKER(LUARegisterState, LUA_REGISTER, TYPE_LUA_REGISTER)
+
+static Property lua_regitser_properties[] = {
+	DEFINE_PROP_STRING("name", LUARegisterState, name),
+	DEFINE_PROP_UINT32("default", LUARegisterState, rstval, 0),
+	DEFINE_PROP_END_OF_LIST(),
+};
+
+static uint64_t lua_register_read(void *opaque, hwaddr offset, unsigned int size)
+{
+	LUARegisterState *s = LUA_REGISTER(opaque);
+
+	return s->rstval;
+}
+
+static void lua_register_write(void *opaque, hwaddr offset,
+                              uint64_t value, unsigned int size)
+{
+	LUARegisterState *s = LUA_REGISTER(opaque);
+
+	s->rstval = value;
+}
+
+static const MemoryRegionOps lua_register_ops = {
+	.read = lua_register_read,
+	.write = lua_register_write,
+	.endianness = DEVICE_LITTLE_ENDIAN,
+	.impl.min_access_size = 4,
+	.impl.max_access_size = 4,
+};
+
+static void lua_regitser_realize(DeviceState *dev, Error **errp)
+{
+	LUARegisterState *s = LUA_REGISTER(dev);
+
+	if (s->name == NULL) {
+		error_setg(errp, "property 'name' not specified");
+		return;
+	}
+
+	memory_region_init_io(&s->mmio, OBJECT(s), &lua_register_ops,
+	              s, s->name, sizeof(uint32_t));
+	sysbus_init_mmio(SYS_BUS_DEVICE(s), &s->mmio);
+}
+
+static void lua_regitser_class_init(ObjectClass *klass, void *data)
+{
+	DeviceClass *dc = DEVICE_CLASS(klass);
+
+	device_class_set_props(dc, lua_regitser_properties);
+	dc->realize = lua_regitser_realize;
+	dc->desc = "Laguna Register With Default Value";
+}
+
+static void lua_regitser_init(Object *obj)
+{
+}
+
+static const TypeInfo lua_regitser_info = {
+	.name = TYPE_LUA_REGISTER,
+	.parent = TYPE_SYS_BUS_DEVICE,
+	.instance_size = sizeof(LUARegisterState),
+	.instance_init = lua_regitser_init,
+	.class_init = lua_regitser_class_init
+};
+
+static void lua_regitser_register_types(void)
+{
+	type_register_static(&lua_regitser_info);
+}
+type_init(lua_regitser_register_types)
